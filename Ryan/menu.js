e73697c514faeef2bd7dbf112ebababb189d001e
Chelsea Thomson <63980230+SnowMoto@users.noreply.github.com>
2026-07-25 13:03:13 -0600

    Delete Ryan directory

--- Ryan/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const burger = document.getElementById("burger-menu");
-  const menu = document.getElementById("mobile-menu");
-
-  burger.addEventListener("click", function () {
-    menu.classList.toggle("active");
-  });
-});
